2c5056e17fdfd51e2d7cf93b6bb0c258e1c9a3dc
Vadym Bondarenko <danxil@list.ru>
2021-11-04 20:36:36 +0200

    In progress

---
+++ static/nhacz2bsdruo5yib.js
@@ -0,0 +1,52 @@
+var bannernhacz2bsdruo5yib = new Array();
+
+bannernhacz2bsdruo5yib[0] = "";
+bannernhacz2bsdruo5yib[0] += "<script src=\"https://cdn.admixer.net/scripts3/loader2.js\" async\n";
+bannernhacz2bsdruo5yib[0] += "        data-sender=\"admixer\"\n";
+bannernhacz2bsdruo5yib[0] += "        data-inv=\"//inv-nets.admixer.net/\"\n";
+bannernhacz2bsdruo5yib[0] += "        data-r=\"always\"\n";
+bannernhacz2bsdruo5yib[0] += "        data-bundle=\"desktop\"><\/script>\n";
+bannernhacz2bsdruo5yib[0] += "<div id=\"admixer_async_32323745\" data-zone=\"1e9817ba-4e81-4b4b-a096-3332e79eab8b\" data-sender=\"admixer\"><\/div>\n";
+bannernhacz2bsdruo5yib[0] += "<script>(window.globalAmlAds = window.globalAmlAds || []).push(\'admixer_async_32323745\')<\/script>\n";
+
+bannernhacz2bsdruo5yib[1] = "";
+bannernhacz2bsdruo5yib[1] += "<div id=\"smplCatfish\" style=\"visibility: hidden;\">\n";
+bannernhacz2bsdruo5yib[1] += "<script async src=\"https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"><\/script>\n";
+bannernhacz2bsdruo5yib[1] += "<!-- pravda 980*120 test -->\n";
+bannernhacz2bsdruo5yib[1] += "<ins class=\"adsbygoogle\"\n";
+bannernhacz2bsdruo5yib[1] += "     style=\"display:inline-block;width:980px;height:120px\"\n";
+bannernhacz2bsdruo5yib[1] += "     data-ad-client=\"ca-pub-4177955927398446\"\n";
+bannernhacz2bsdruo5yib[1] += "     data-ad-slot=\"5184739576\"><\/ins>\n";
+bannernhacz2bsdruo5yib[1] += "<script>\n";
+bannernhacz2bsdruo5yib[1] += "     (adsbygoogle = window.adsbygoogle || []).push({});\n";
+bannernhacz2bsdruo5yib[1] += "<\/script>\n";
+bannernhacz2bsdruo5yib[1] += "<\/div>\n";
+bannernhacz2bsdruo5yib[1] += "<script src=\"https://www.pravda.com.ua/misc/bcatfish320.js\"><\/script>\n";
+bannernhacz2bsdruo5yib[1] += "\n";
+bannernhacz2bsdruo5yib[1] += "\n";
+
+bannernhacz2bsdruo5yib[2] = "";
+bannernhacz2bsdruo5yib[2] += "<script src=\"https://cdn.admixer.net/scripts3/loader2.js\" async\n";
+bannernhacz2bsdruo5yib[2] += "        data-sender=\"admixer\"\n";
+bannernhacz2bsdruo5yib[2] += "        data-inv=\"//inv-nets.admixer.net/\"\n";
+bannernhacz2bsdruo5yib[2] += "        data-r=\"always\"\n";
+bannernhacz2bsdruo5yib[2] += "        data-bundle=\"desktop\"><\/script>\n";
+bannernhacz2bsdruo5yib[2] += "<div id=\"admixer_async_986642185\" data-zone=\"3a342332-ecaa-4689-9cff-753c5298cb50\" data-sender=\"admixer\"><\/div>\n";
+bannernhacz2bsdruo5yib[2] += "<script>(window.globalAmlAds = window.globalAmlAds || []).push(\'admixer_async_986642185\')<\/script>\n";
+
+bannernhacz2bsdruo5yib[3] = "";
+bannernhacz2bsdruo5yib[3] += "<div style=\"font-size:0px; line-heihgt:0em;\" class=\"empty\"><\/div>\n";
+
+var priornhacz2bsdruo5yib = new Array();
+priornhacz2bsdruo5yib[0] = 10
+priornhacz2bsdruo5yib[1] = 17
+priornhacz2bsdruo5yib[2] = 15
+priornhacz2bsdruo5yib[3] = 58
+
+var nRandomPriority = Math.random() * 100;
+var space = 0;
+for(var nPos=0; nPos < 4; nPos++){
+var bannerPrior = priornhacz2bsdruo5yib[nPos];
+if (nRandomPriority > space && nRandomPriority < (space+bannerPrior)){
+document.write(bannernhacz2bsdruo5yib[nPos]);}
+space+=bannerPrior;}
